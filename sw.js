e74c6d96e2a1b3d348abcda0fffead5296dc8194
Spart0n <avizza88@gmail.com>
2023-09-30 17:53:19 -0700

    fetch added to the SW

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 //service workers only get installeds once  in their live cycle 
 
-self.addEventListener("intstall", e =>{
-    console.log("Install!");
+self.addEventListener("intstall", e => {
+  e.waitUntil(
+    caches.open("static").then(cache => {
+        return cache.addAll(["./","./css/master.css","./images/logo.png"])
+    })
+  )
+})
+
+self.addEventListener("fetch", e => {
+    console.log('Intercepting fetch request for: ${e.request.url}');
 })
